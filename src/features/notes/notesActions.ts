8510b655c9eed21eed97af2d5e17eb002653a63c
Daniel-Santiago-Acosta-1013 <santiagoacosta1013@gmail.com>
2023-05-03 20:39:45 -0500

    [add] agregando estilos a las cajitas de notas y demas

--- src/features/notes/notesActions.ts
+++ src/features/notes/notesActions.ts
@@ -2,28 +2,34 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 import { Note } from "./types";
 
+const baseUrl = 'http://localhost:4004';
+
+const headers = {
+  'Authorization': 'Bearer your_token_here',
+  // Añade más headers aquí si es necesario
+};
+
 export const fetchNotes = createAsyncThunk('notes/fetchNotes', async () => {
-  const response = await axios.get('/api/notes');
+  const response = await axios.get(`${baseUrl}/notes`, { headers });
   return response.data as Note[];
 });
 
-
 export const fetchNote = createAsyncThunk('notes/fetchNote', async (id: string) => {
-  const response = await axios.get(`/api/notes/${id}`);
+  const response = await axios.get(`${baseUrl}/notes/${id}`, { headers });
   return response.data;
 });
 
 export const createNote = createAsyncThunk('notes/createNote', async (note: any) => {
-  const response = await axios.post('/api/notes', note);
+  const response = await axios.post(`${baseUrl}/notes`, note, { headers });
   return response.data;
 });
 
 export const updateNote = createAsyncThunk('notes/updateNote', async (note: any) => {
-  const response = await axios.put(`/api/notes/${note.id}`, note);
+  const response = await axios.put(`${baseUrl}/notes/${note.id}`, note, { headers });
   return response.data;
 });
 
 export const deleteNote = createAsyncThunk('notes/deleteNote', async (id: string) => {
-  await axios.delete(`/api/notes/${id}`);
+  await axios.delete(`${baseUrl}/notes/${id}`, { headers });
   return id;
 });
